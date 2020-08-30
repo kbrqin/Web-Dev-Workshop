cb9233819240573f00432eb11cd311ce19cbabcb
kbrqin <kbrqin@gmail.com>
2020-08-30 17:38:44 -0400

    personalize site

--- personal_site/settings/__init__.py
+++ personal_site/settings/__init__.py
@@ -30,7 +30,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
